3708754ded366d3676b8f8e68a03d12a503b3283
Cheton Wu <cheton@gmail.com>
2017-01-20 19:32:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# typography [![build status](https://travis-ci.org/trendmicro-frontend/typography.svg?branch=master)](https://travis-ci.org/trendmicro-frontend/typography) [![Coverage Status](https://coveralls.io/repos/github/trendmicro-frontend/typography/badge.svg?branch=master)](https://coveralls.io/github/trendmicro-frontend/typography?branch=master)
+
+[![NPM](https://nodei.co/npm/@trendmicro/typography.png?downloads=true&stars=true)](https://nodei.co/npm/@trendmicro/typography/)
+
+React Typography
+
+Demo: https://trendmicro-frontend.github.io/typography
+
+## Installation
+
+1. Install the latest version of [react](https://github.com/facebook/react) and [typography](https://github.com/trendmicro-frontend/typography):
+
+  ```
+  npm install --save react @trendmicro/typography
+  ```
+
+2. Install [typography](https://github.com/trendmicro-frontend/typography)` with <b>@trendmicro</b> scope:
+
+  ```js
+  import Typography from '@trendmicro/typography';
+  ```
+
+## Usage
+
+```js
+<Typography
+    className="component"
+>
+    GitHub
+</Typography>
+```
+
+
+## API
+
+### Properties
+
+<table>
+  <thead>
+    <tr>
+      <th align="left">Name</th>
+      <th align="left">Type</th>
+      <th align="left">Default</th>
+      <th align="left">Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+
+## License
+
+MIT
---
+++ examples/index.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import '../dist/css/index.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    <div className="typography-example">
+                        <h3>Fonts</h3>
+                        <p>Ensure to use <strong>Interstate</strong> as the primary typeface in product UI. For text that is 18px or smaller, use <strong>Arial</strong>.</p>
+                        <div className="row">
+                            <label className="text-muted">Display 4</label>
+                            <span className="display4">Interstate-ExtraLight 40px</span>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">Display 3</label>
+                            <span className="display3">Interstate-Light 32px</span>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">H1, Display 2</label>
+                            <span className="display2">Interstate-Light 26px</span>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">H2, Display 1</label>
+                            <span className="display1">Interstate-Light 24px</span>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">H3, Title</label>
+                            <h3>Interstate-Light 18px</h3>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">H4, Large Text</label>
+                            <h4>Arial 16px</h4>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">H5</label>
+                            <h5>Arial 14px Bold</h5>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">H6</label>
+                            <h6>Arial 13px Bold</h6>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">Body</label>
+                            <span>Arial 13px</span>
+                        </div>
+                        <div className="row">
+                            <label className="text-muted">Small Text</label>
+                            <span className="font-size-small">Arial 12px</span>
+                        </div>
+                        <div className="example">
+                            <div className="typography-example">
+                                <p>Use a smaller font to create a secondary heading. We often see this in our product names.</p>
+                                <h1>Trend Micro Style <small>for Commercial</small></h1>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('container')
+);
