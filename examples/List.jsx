7752504181c12294e6e8f20269e285a6a31361e9
Cheton Wu <cheton@gmail.com>
2017-03-01 18:15:54 +0800

    Fix an issue that <h3> appear as a child of <h3>

--- examples/List.jsx
+++ examples/List.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default () => {
     return (
         <section>
-            <h3 style={{ margin: '16px 0' }}>
-                <h3>List</h3>
-            </h3>
+            <div style={{ margin: '16px 0' }}>
+                List
+            </div>
             <div className="typography-example">
                 <div className="lists-unordered">
                     <h6>Unordered lists</h6>
